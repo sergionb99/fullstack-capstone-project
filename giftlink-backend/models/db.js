aa6113eef1b4d40d1bfbc2cb86bb402247a93027
sergionb99 <sergionb1999@gmail.com>
2025-07-02 04:31:41 -0400

    Create the MainPage and Navbar

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,17 @@
 // db.js
-require('dotenv').config();
+require('dotenv').config({ path: './util/import-mongo/.env' });
 const MongoClient = require('mongodb').MongoClient;
-
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
-
 let dbInstance = null;
 const dbName = "giftdb";
-
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
-
-    const client = new MongoClient(url);      
-
-    // Task 1: Connect to MongoDB
+    const client = new MongoClient(url);
     await client.connect();
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    const dbInstance = client.db(giftDB);
-
-    // Task 3: Return database instance
+    dbInstance = client.db(dbName);
     return dbInstance;
 }
-
 module.exports = connectToDatabase;
